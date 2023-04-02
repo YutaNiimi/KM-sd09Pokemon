3267c0f2660459f3b72ebeaf40428c2058304acf
YutaNiimi <122436613+YutaNiimi@users.noreply.github.com>
2023-04-02 11:09:05 +0900

    Update pokemon.vue

--- pages/trainer/[name]/pokemon.vue
+++ pages/trainer/[name]/pokemon.vue
@@ -27,6 +27,8 @@ const { dialog, onOpen, onClose } = useDialog();
     <div>
         <h1>トレーナー情報</h1>
         <span style="font-size:25">{{ $route.params.name }}</span>
+        <p></p>
+    　　<button type="button" onclick="history.back()" >戻る</button>
         <div style="padding: 10px; margin-bottom: 10px;width:500px ; border: 5px solid #333333; border-radius: 5px;">
                 <GamifyItem v-for="pokemon in pokemons.results" :key="pokemon.url">
                 <span class="pokemon-name">{{ pokemon.name }}</span>
